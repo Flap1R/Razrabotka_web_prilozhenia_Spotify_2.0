cbc00304f5a35bf777a2fd74306472bdaf3bbf01
flap1r <isamolazov@gmail.com>
2023-12-23 19:45:55 +0300

    feat: add sidebar

---
+++ client/src/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = (children) => {
+  return (
+    <div className="flex">
+        <Sidebar/>
+        {children}
+    </div>
+  );
+};
+
+export default Layout
---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {BiLibrary, BiSolidHome} from "react-icons/bi";
+import {FiSearch} from "react-icons/fi";
+import {FaPlus} from "react-icons/fa";
+import {TbWorld} from "react-icons/tb";
+const Sidebar = () => {
+    return (
+        <div className="w-1/4 sidebar ">
+            <div className="nav secondary_bg rounded-lg p-6">
+                <div className="flex items-center gap-4">
+                    <BiSolidHome className="font-bold text-xl"/>
+                    <span>Home</span>
+                </div>
+                <div className="flex items-center gap-4">
+                    <FiSearch className="font-bold text-xl"/>
+                    <span>Search</span>
+                </div>
+            </div>
+            <div className="your_library mt-2 secondary_bg rounded-lg px-2 py-2">
+                <div className="flex px-4 justify-between mb-4 items-center gap-4">
+                    <div className="flex gap-2 items-center">
+                    <BiLibrary className="font-bold text-xl"/>
+                    <span>Your library</span>
+                    </div>
+                    <button className="hover:bg-black/25 rounded-[50%] p-2">
+                        <FaPlus className="font-bold text-xl"/>
+                    </button>
+                </div>
+                <div className="your_library leading-8 mt-2 tertiary_bg rounded-lg py-6 px-4">
+                    <p className="font-bold">Create your first playlist</p>
+                    <p className="font-semibold">It's easy, we'll help you</p>
+                    <buttonn className="rounded-full text-black mt-4 px-4 py-0 bg-white font-semibold">
+                        Create playlist
+                    </buttonn>
+                </div>
+                <div className="your_library leading-8 mt-4 tertiary_bg rounded-lg py-6 px-4">
+                    <p className="font-bold">Lets find some podcast to follow</p>
+                    <p className="font-semibold">We'll keep you updated on new episodes</p>
+                    <buttonn className="rounded-full text-black mt-4 px-4 py-0 bg-white font-semibold">
+                        Browse Podcast
+                    </buttonn>
+                </div>
+            </div>
+            <div className="mt-4 px-4 flex gap-4 flew-wrap">
+                <a className="text-xs text-gray-300 mx-4" href="#">Legal</a>
+                <a className="text-xs text-gray-300 mx-4" href="#">Privacy Center</a>
+                <a className="text-xs text-gray-300 mx-4" href="#">Privacy Policy</a>
+                <a className="text-xs text-gray-300 mx-4" href="#">Cookies</a>
+                <a className="text-xs text-gray-300 mx-4" href="#">About Ads</a>
+                <a className="text-xs text-gray-300 mx-4" href="#">Accessibility</a>
+            </div>
+            <button className="mx-4 mt-4 text-sm border-white border rounded-full flex gap-2 px-3 py-1 item-center text-white">
+                <TbWorld />
+                <span className="text-white font-bold">English</span>
+            </button>
+        </div>
+  );
+};
+
+export default Sidebar
